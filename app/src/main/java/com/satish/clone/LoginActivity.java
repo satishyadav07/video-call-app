624c3a88b4e388a996eb7ac958515a9247cd34c1
satishyadav07 <satishyadav25218@gmail.com>
2023-12-29 11:15:42 +0530

    first commit

---
+++ app/src/main/java/com/satish/clone/LoginActivity.java
@@ -0,0 +1,48 @@
+package com.satish.clone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.firebase.FirebaseApp;
+import com.hbb20.CountryCodePicker;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    CountryCodePicker countryCodePicker;
+    EditText phoneInput;
+    Button sendOtpBtn;
+    ProgressBar progressBar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        countryCodePicker=findViewById(R.id.login_countryCode);
+        phoneInput=findViewById(R.id.login_mobile_number);
+        sendOtpBtn=findViewById(R.id.sendOtpBtn);
+        progressBar=findViewById(R.id.login_progress_bar);
+
+        progressBar.setVisibility(View.GONE);
+
+
+        countryCodePicker.registerCarrierNumberEditText(phoneInput);
+        sendOtpBtn.setOnClickListener(view -> {
+            if(!countryCodePicker.isValidFullNumber()){
+                phoneInput.setError("Phone number not valid");
+                return;
+            }
+
+            Intent intent=new Intent(LoginActivity.this,LoginOtpActivity.class);
+            intent.putExtra("phone",countryCodePicker.getFullNumberWithPlus());
+            startActivity(intent);
+        });
+    }
+}
---
+++ app/src/main/java/com/satish/clone/MyApplication.java
@@ -0,0 +1,15 @@
+package com.satish.clone;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+
+    }
+}
